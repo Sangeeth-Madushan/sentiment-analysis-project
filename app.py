4212205a5be6e9bfc413fcba335779c2dd698ebd
sangeeth <sangeeth99614@gmail.com>
2025-09-16 13:27:47 +0530

    integrate model and web app

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request, redirect
+from helper import preprocessing, vectorizer, get_prediction
 
 
 app = Flask(__name__)
 
 data = dict()
-reviews = ['Great product!', 'Not what I expected.','would buy again', 'poor quality', 'excellent value for money']
-positive = 3
-negative = 2
+reviews = []
+positive = 0
+negative = 0
 
 @app.route("/")
 def index():
@@ -14,6 +15,29 @@ def index():
     data['positive'] = positive
     data['negative'] = negative
     return render_template('index.html', data=data)
+
+@app.route("/", methods = ['post'])
+def my_post():
+    text = request.form['text']
+
+    preprocessed_txt = preprocessing(text)
+
+
+    vectorized_txt = vectorizer(preprocessed_txt)
+
+
+    prediction = get_prediction(vectorized_txt)
+
+
+    if prediction == 'negative':
+        global negative
+        negative += 1
+    else:
+        global positive
+        positive += 1
+    
+    reviews.insert(0, text)
+    return redirect(request.url)
 
 
 if __name__ == "__main__":
